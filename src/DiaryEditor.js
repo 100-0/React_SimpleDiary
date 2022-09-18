0df130824ab8701537779f45a9b779816aa74084
100-0 <back-zero@naver.com>
2022-09-18 22:57:35 +0900

    1.사용자입력처리

---
+++ src/DiaryEditor.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+    const [state, setState] = useState({
+        author: "",
+        content: "",
+        emotion: 1,
+    });
+
+    const handleChangeState = (e) => {
+        setState({
+            ...state,
+            /*괄호표기법. 입력한 것이 content면, [content]: content의 value로 들어감!
+            author창을 입력하면, [author]: author의 value!*/
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = () => {
+        console.log(state);
+        alert("저장성공");
+    };
+    
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    name="author"
+                    value={state.author} 
+                    onChange={handleChangeState}
+                />
+            </div>
+            <div>
+                <textarea
+                    name="content"
+                    value={state.content}
+                    onChange={handleChangeState}
+                />
+            </div>
+            <div>
+                <span>오늘의 감정점수 : </span>
+                <select 
+                    name="emotion"
+                    value={state.emotion}
+                    onChange={handleChangeState}
+                >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handleSubmit}>일기 저장하기</button>
+            </div>
+        </div>
+    );
+};
+export default DiaryEditor;
