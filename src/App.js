deb69431a730b30eb3b4e7f62208abb2faf2f861
100-0 <back-zero@naver.com>
2022-10-05 19:54:34 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useMemo, useState, useRef, useEffect } from "react";
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import './App.css';
@@ -44,7 +44,6 @@ const App = () => {
   }
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
@@ -57,9 +56,22 @@ const App = () => {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+      const goodCount = data.filter((it) => it.emotion >= 3).length;
+      const badCount = data.length - goodCount;
+      const goodRatio = (goodCount / data.length) * 100;
+      return {goodCount, badCount, goodRatio};
+    }, [data.length]);
+
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length} </div>
+      <div>기분 좋은 일기 개수 : {goodCount} </div>
+      <div>기분 나쁜 일기 개수 : {badCount} </div>
+      <div>기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
